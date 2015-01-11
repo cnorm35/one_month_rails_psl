eb8abacd12ea038c6d822cc257cb88afa0884804
Cody <wcodynorman@gmail.com>
2015-01-11 15:15:48 -0500

    added settings and configuration for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,10 +22,15 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #Required for Heroku
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
